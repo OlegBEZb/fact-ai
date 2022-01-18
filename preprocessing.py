59b1c544c76bdaaef17aa441cc81742e3504124a
Yuri <y.a.sturkenboom@gmail.com>
2022-01-18 02:56:01 +0100

    Speed up preprocessing by factor 8

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,28 @@
+from html import entities
 import pardata
 import spacy
 from collections import Counter
+from tqdm import tqdm
 
-sp = spacy.load('en_core_web_sm')
+# Only use the PoS tagger, or processing will take very long
+nlp = spacy.load('en_core_web_sm', disable=[
+    'parser',
+    'entity',
+    'ner',
+    'entity_linker',
+    'entity_ruler',
+    'textcat',
+    'textcat_multilabel',
+    'morphologizer',
+    'senter',
+    'sentencizer',
+    'tok2vec',
+    'transformers'
+])
 
+print('Loading dataset...')
 wikitext103_data = pardata.load_dataset('wikitext103')
+print('done')
 
 MIN_TOKEN_FREQ = 10
 
@@ -21,25 +39,27 @@ def preprocess(dataset):
     # Accumulate unigram POS tags
     doc_tokens = []
     text_tokens = []
-    for line in lower_lines:
-        pos_tagged = sp(line)
-        for token in pos_tagged:
+
+    for doc in tqdm(nlp.pipe(lower_lines), total=len(lower_lines)):
+        for token in doc:
 
             # append single token with text, PoS tag and more to list of all tokens
             doc_tokens.append(token)
+
+            # append just token text to a list to later count term frequencies
             text_tokens.append(token.text)
 
     # Remove infrequent terms
 
     # Gather token frequencies
-    tokens_freqs = dict(Counter(text_tokens))
+    tokens_freqs = dict(Counter(tqdm(text_tokens)))
 
     # Filter out tokens that occur less than the minimum word frequency
-    frequent_tokens = list(filter(lambda token: tokens_freqs[token.text] >= 10, doc_tokens))
+    frequent_tokens = list(filter(lambda token: tokens_freqs[token.text] >= 10, tqdm(doc_tokens)))
 
     return frequent_tokens
 
 if __name__ == '__main__':
     tokens = preprocess(wikitext103_data['train'])
     print(f'Sample token: \nText: {tokens[40].text} \nPoS tag: {tokens[40].tag_}')
-    
+
