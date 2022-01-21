ca35a009bffb60e0ab23792bbb624243ebc48861
Yuri <y.a.sturkenboom@gmail.com>
2022-01-21 20:12:53 +0100

    Preprocessing supports saving progress using pickling

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,11 @@
+from cmath import log
 from html import entities
 import pardata
 import spacy
+from spacy.tokens import DocBin
 from collections import Counter
 from tqdm import tqdm
+import pickle
 
 # Only use the PoS tagger, or processing will take very long
 nlp = spacy.load('en_core_web_sm', disable=[
@@ -26,7 +29,7 @@ print('done')
 
 MIN_TOKEN_FREQ = 10
 
-def preprocess(dataset):
+def preprocess(dataset, doc_bin_filename):
 
     # Split raw text dataset into individual lines 
     lines = dataset.splitlines()
@@ -36,30 +39,50 @@ def preprocess(dataset):
 
     # lower_lines = lower_lines[:1000]
 
-    # Accumulate unigram POS tags
-    doc_tokens = []
-    text_tokens = []
+    # Accumulate docs for pickling
+    # We can only pickle (serialize) docs unfortunately
+    doc_bin = DocBin(store_user_data=True)
 
     for doc in tqdm(nlp.pipe(lower_lines), total=len(lower_lines)):
-        for token in doc:
+        doc_bin.add(doc)
 
-            # append single token with text, PoS tag and more to list of all tokens
-            doc_tokens.append(token)
+    # Save docs with tokens
+    with open(f'{doc_bin_filename}_doc_bin.pickle', 'wb') as handle:
+        pickle.dump(doc_bin, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-            # append just token text to a list to later count term frequencies
-            text_tokens.append(token.text)
+def tokens_from_serialized_docs(filename):
+    # Read docs with tokens
 
-    # Remove infrequent terms
+    with open(f'{filename}_doc_bin.pickle', 'rb') as handle:
+        print('Loading docs...')
+        doc_bin = pickle.load(handle)
+        docs = list(doc_bin.get_docs(nlp.vocab))
+        print('done')
 
-    # Gather token frequencies
-    tokens_freqs = dict(Counter(tqdm(text_tokens)))
+        # Accumulate unigram POS tags
+        doc_tokens = []
+        text_tokens = []
 
-    # Filter out tokens that occur less than the minimum word frequency
-    frequent_tokens = list(filter(lambda token: tokens_freqs[token.text] >= 10, tqdm(doc_tokens)))
+        for doc in docs:
+            for token in doc:
+                # append single token with text, PoS tag and more to list of all tokens
+                doc_tokens.append(token)
 
-    return frequent_tokens
+                # append just token text to a list to later count term frequencies
+                text_tokens.append(token.text)
+        
+        # Remove infrequent terms
+        # Gather token frequencies
+        tokens_freqs = dict(Counter(text_tokens))
+
+        # Filter out tokens that occur less than the minimum word frequency
+        print('Filtering infrequent tokens...')
+        frequent_tokens = list(filter(lambda token: tokens_freqs[token.text] >= 10, tqdm(doc_tokens)))
+        print('done')
+
+        return frequent_tokens
 
 if __name__ == '__main__':
-    tokens = preprocess(wikitext103_data['train'])
+    # preprocess(wikitext103_data['train'], 'wikitext')
+    tokens = tokens_from_serialized_docs('wikitext')
     print(f'Sample token: \nText: {tokens[40].text} \nPoS tag: {tokens[40].tag_}')
-
